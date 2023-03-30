364fc629e84cb570e5b82daa10af2386491b4eeb
saleh jalili <salehjalili36@gmail.com>
2023-03-31 03:01:56 +0330

    ecommrce

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import Footer from '@/components/layout/footer'
+import Header from '@/components/layout/header'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin']
+})
+
+export const metadata = {
+  title: 'next ecommerce',
+  description: 'Created by Hamed Bahram'
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html
+      lang='en'
+      className={`${inter.className} h-full scroll-smooth antialiased`}
+    >
+      <body className='flex h-full flex-col text-stone-700'>
+        <Header />
+        <main className='grow '>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
